43500ac1f37b83a0b633a02467db03ddfa9f006f
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-11-08 09:41:12 -0300

    starting-main

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ var btnMobileLeft = document.querySelector(".btn-mobile-left")
 var menuLeft = document.querySelector(".nav-list-mobile")
 var menuSacola = document.querySelector(".bag-products")
 var btnSacola = document.querySelector("#mobile-sac")
+var btnClose = document.querySelector("#btn-close-left")
+var imageSplash = document.querySelector("#splash")
+
+imageSplash.addEventListener("load", (e) => {
+    
+    setTimeout( () => {
+        imageSplash.style.display = "none"
+    }, 3000)
+    
+})
 
 btnMobileLeft.onclick = () => {
     menuLeft.classList.toggle("esconder-left")
 }
+
+btnClose.addEventListener("click", () => {
+    menuLeft.style.display = "none"
+
+   //  alert("teste")
+   location.reload()
+})
 
 btnSacola.onclick = () => {
     menuSacola.classList.toggle("esconder")
